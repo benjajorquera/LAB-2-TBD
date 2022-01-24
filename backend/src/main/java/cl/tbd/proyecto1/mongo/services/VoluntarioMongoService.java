58edf6ba1316142041b6aa69bf3356ee0022f8ef
jgonzalezk <jorge.gonzalez.q@usach.cl>
2022-01-23 21:32:34 -0300

    getOne, deleteOne y updateOne "Voluntario"

--- backend/src/main/java/cl/tbd/proyecto1/mongo/services/VoluntarioMongoService.java
+++ backend/src/main/java/cl/tbd/proyecto1/mongo/services/VoluntarioMongoService.java
@@ -1,10 +1,13 @@
 package cl.tbd.proyecto1.mongo.services;
 
 import java.util.List;
+import java.util.Date;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -42,4 +45,36 @@ public class VoluntarioMongoService {
         return String.format("Los voluntarios tienen en promedio %s habilidades.", total);
     }
 
+    // @GetMapping("/mongo/voluntarios/{id}")
+    // public VoluntarioMongo getVoluntario(@PathVariable Integer id){
+    //     return voluntarioRepository.getVoluntario(id);
+    // }
+
+    // @PutMapping("/mongo/voluntarios")
+    // public VoluntarioMongo updateHabilidad(@RequestParam Integer id, @RequestParam(required = false) String nombre, @RequestParam(required = false) Date fnacimiento){
+    //     VoluntarioMongo voluntarioAux = getVoluntario(id);
+    //     if(voluntarioAux == null){
+    //         return null;
+    //     }else{
+    //         if(nombre == null){
+    //             codigo = voluntarioAux.getNombre();
+    //         }
+    //         if(descrip == null){
+    //             descrip = voluntarioAux.getFNacimiento();
+    //         }
+    //     }
+    //     VoluntarioMongo result = VoluntarioMongoRepository.updateVoluntario(voluntarioAux);
+    //     return result;
+    // }
+
+    //Delete
+    // @DeleteMapping("/mongo/voluntarios/delete/{id}")
+    // public String deleteVoluntario(@PathVariable Integer id){     
+    //     VoluntarioMongo aux = getVoluntario(id);
+    //     if(aux == null){
+    //         return String.format("El voluntario con id = %s no existe.", id);
+    //     }
+    //     VoluntarioMongo result = voluntarioRepository.deleteVoluntario(id);
+    //     return String.format("El voluntario con id = %s se elimino, junto a sus dependencias.", id);
+    // }
 }
